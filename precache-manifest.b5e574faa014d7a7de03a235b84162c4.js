f0350d055f0d363d8f85a07b2ebe3cb0e603a38a
Valentine <valentinezavizion@gmail.com>
2021-06-14 11:42:06 +0300

    Added prod code

---
+++ precache-manifest.b5e574faa014d7a7de03a235b84162c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc034be694b865874e8d",
+    "url": "/static/css/main.f295c00b.chunk.css"
+  },
+  {
+    "revision": "cc034be694b865874e8d",
+    "url": "/static/js/main.5dad4c6f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e0e5fe6cee356015edf",
+    "url": "/static/js/2.a25a2fc3.chunk.js"
+  },
+  {
+    "revision": "6a14a9de0c31a0216f4a62111a5a987e",
+    "url": "/index.html"
+  }
+];
